ecd30b2ba116b85cbe5e7a08ca96e7f734a4ffac
catOWO2011 <catOWO2011@gmail.com>
2023-11-15 20:28:39 -0400

    Added function to show volunteer list page and hide volunteer home page

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 // Function to show home page
 
 // Function to show volunteer list page
-
+function showVolunteerListPage() {
+  const voluteerSearchPage = document.getElementById("volunteer_search_page");
+  voluteerSearchPage.style.display = 'block';
+  const voluteerSearchHome = document.getElementById("volunteer_search_home");
+  voluteerSearchHome.style.display = 'none';
+}
 // Function to show volunteer details page 1
 
 // Function to show volunteer details page 2
