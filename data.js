65dd58fdf117ac4aece0de8de016aaa5466209d9
Jim <jamesgabrielpeterson@gmail.com>
2023-12-20 20:04:03 -0500

    homeView to MVC

---
+++ data.js
@@ -0,0 +1,171 @@
+export const volunteer_list = [
+    {
+      id: 0,
+      name: "Suicide Hotline",
+      image: {
+        url: "https://www.arkansashouse.org/storage/15950/2022_Suicide-Crisis-Lifeline%20copy.jpg", 
+        description: "Picture of the 988 Hotline"
+      },
+      location: {
+        address: "123 Main Street",
+        city: "Rocky Hill",
+        state: "CT",
+        zip: "06067"
+      },
+      date_created: new Date(),
+      date_hosted: new Date(),
+      main: ["Mental Health", "Crisis Hotline"],
+      mission: "Our mission is to ...", 
+      description:
+        "This nonprofit trains members of the community to pick up calls and talk to people.",
+      cause_areas: ["Advocacy", "Community"],
+      skills: ["Communication", "People Skills", "Social Work"],
+      good_for: "Group",
+      requirements: ["Driver's License", "Background Check", "Must be at least 18", "Training", "Very Flexible, any support is appreciated"]
+    },
+    {
+      id: 1,
+      name: "Own My Puppy",
+      image:  {
+        url: "https://variety.com/wp-content/uploads/2014/01/puppy_bowl_lineup.jpg?w=1000",
+        description: "Pictures of pups"
+      },
+      location: {
+        address: "456 Main Street",
+        city: "Reston",
+        state: "Virginia",
+        zip: "20190"
+      },
+      date_created: new Date(),
+      date_hosted: new Date(),
+      main: ["Pet companion", "Lonely souls", "Animal Rescue"],
+      mission: "Our mission is to ...", 
+      description: 
+         "This is non-profit org which works with Pet experts",
+      cause_areas: ["Advocacy", "Community"],
+      skills: ["Communication", "People Skills", "Social Work"],
+      good_for: "Group",
+      requirements: ["Requirement 1", "Requirement 2"]
+    },
+    {
+      id: 2,
+      name: "NSF-NCDC",
+      image: {
+        url: "https://beforetheirtime.org/cdn/shop/articles/what-is-a-nonprofit-organization_1600x.jpg?v=1650520340",
+        description: "Generic Nonprofit Image"
+      },
+      location: {
+        address: "100 Abby Lane",
+        city: "Syosset",
+        state: "NY",
+        zip: "11797",
+      },
+      date_created: new Date(),
+      date_hosted: new Date(),
+      main: ["Pet companion", "Lonely souls", "Animal Rescue"],
+      mission: "Our mission is to ...", 
+      description:
+          "This is non-profit org which works with Climate",
+      cause_areas: ["Advocacy", "Community"],
+      skills: ["Communication", "People Skills", "Social Work"],
+      good_for:["Group"],
+      requirements:["Requirement 1", "Requirement 2"],
+    },
+    {
+      id: 3,
+      name: "Forver Paws Animal Shelter",
+      image: {
+        url: "https://www.raisely.com/blog/content/images/2023/01/Asset-1-1.png",
+        description: "Generic Nonprofit Image"
+      },
+      location: {
+        address: "123 Main Street",
+        city: "Fall River",
+        state: "MA",
+        zip: "02121"
+      },
+      date_created: new Date(),
+      date_hosted: new Date(),
+      main: ["Animal Rescue", "Pet Adoption"],
+      mission: "Our mission is to ...", 
+      description:
+        "This nonprofit gives animals a second chance at finding a furever home.",
+      cause_areas: ["Advocacy", "Community"],
+      skills: ["Communication", "People Skills", "Social Work"],
+      good_for: "Group",
+      requirements: ["Requirement 1", "Requirement 2"]
+    },
+    {
+      id: 4,
+      name: "Gift of Bread",
+      image: {
+        url: "https://www.glenroybakery.com.au//wp-content/uploads/2023/01/Wholesale-Bread-Glenroy-Bakery.jpg", 
+        description: "Picture of different types of bread"
+      },
+      location: {
+        address: "79 George Street",
+        city: "Sydney",
+        state: "NSW",
+        zip: "2001"
+      },
+      date_created: new Date(),
+      date_hosted: new Date(),
+      main: ["Sharing bread", "Reduce food waste"],
+      mission: "Committed to building strong, inclusive communities while also minimizing waste and promoting sustainability", 
+      description:
+        "Gift of Bread is an Australian food rescue charitable organisation",
+      cause_areas: ["Sustainability", "Feeding vulnerable people"],
+      skills: ["Baking", "Food handling", "First aid"],
+      good_for: "Group",
+      requirements: ["Availability for night shifts", "Driving Licence"]
+    },
+    {
+      id: 5,
+      name: "American  Red Cross",
+      image:  {
+        url: "https://www.redcross.org/content/dam/redcross/donations/holiday/2023-holiday-catalog/2023-holiday-donate-form-creative/Holiday-2023-WINM-Desktop_1348x505.png.transform/1024/q70/feature/image.png",
+        description: "red cross"
+      },
+  
+      location: {
+        address: "123 South Street",
+        city: "St. Louis",
+        state: "MO",
+        zip: "123456"
+      },
+      date_created: new Date(),
+      date_hosted: new Date(),
+      main: ["Donate blood", "Save lives"],
+      mission: "Our mission is to ...",
+      description:
+        "Humanitarian organization that provides emergency assistance, disaster relief, and disaster preparedness",
+      cause_areas: ["Aid", "Humanitarian"],
+      skills: ["Leadership", "People Skills", "Social Work"],
+      good_for: "Group",
+      requirements: ["good blood", "good heart"],
+    },
+    {
+      id: 6,
+      name: "Canine Sanctuary",
+      image: {
+        url: "https://www.rescuedogs.co.uk/wp-content/uploads/2021/02/pebbles-thumbnail.jpg",
+        description: "Picture of a lovely dog"
+      },
+      location: {
+        address: "42-04 Crescent Ave",
+        city: "Queens",
+        state: "NY",
+        zip: "11106"
+      },
+      date_created: new Date(),
+      date_hosted: new Date(),
+      main: ["Dog Rescue", "Dog Rehabilitiation"],
+      mission: "Our mission is to reduce the instances of dog fighting rings to zero and rescue and rehabilitate dogs.", 
+      description:
+        "This nonprofit investigates dog fighting rings and organizes animal rescue operations. ",
+      cause_areas: ["Animal Rights", "Illegal Operations", "Animal Rehabilitation Center", "Animal Sanctuary"],
+      skills: ["Communication", "Pet Handling", "Social Work", "Military experience", "Law Enforcement Experience"],
+      good_for: "Group",
+      requirements: ["Social work experience", "Animal care experience"]
+    }
+  ];
--- main.js
+++ main.js
@@ -1,192 +1,17 @@
-let volunteer_list = [ 
-  {
-    id: 0,
-    name: "Suicide Hotline",
-    image: {
-      url: "https://www.arkansashouse.org/storage/15950/2022_Suicide-Crisis-Lifeline%20copy.jpg", 
-      description: "Picture of the 988 Hotline"
-    },
-    location: {
-      address: "123 Main Street",
-      city: "Rocky Hill",
-      state: "CT",
-      zip: "06067"
-    },
-    date_created: new Date(),
-    date_hosted: new Date(),
-    main: ["Mental Health", "Crisis Hotline"],
-    mission: "Our mission is to ...", 
-    description:
-      "This nonprofit trains members of the community to pick up calls and talk to people.",
-    cause_areas: ["Advocacy", "Community"],
-    skills: ["Communication", "People Skills", "Social Work"],
-    good_for: "Group",
-    requirements: ["Driver's License", "Background Check", "Must be at least 18", "Training", "Very Flexible, any support is appreciated"]
-  },
-  {
-    id: 1,
-    name: "Own My Puppy",
-    image:  {
-      url: "https://variety.com/wp-content/uploads/2014/01/puppy_bowl_lineup.jpg?w=1000",
-      description: "Pictures of pups"
-    },
-    location: {
-      address: "456 Main Street",
-      city: "Reston",
-      state: "Virginia",
-      zip: "20190"
-    },
-    date_created: new Date(),
-    date_hosted: new Date(),
-    main: ["Pet companion", "Lonely souls", "Animal Rescue"],
-    mission: "Our mission is to ...", 
-    description: 
-       "This is non-profit org which works with Pet experts",
-    cause_areas: ["Advocacy", "Community"],
-    skills: ["Communication", "People Skills", "Social Work"],
-    good_for: "Group",
-    requirements: ["Requirement 1", "Requirement 2"]
-  },
-  {
-    id: 2,
-    name: "NSF-NCDC",
-    image: {
-      url: "https://beforetheirtime.org/cdn/shop/articles/what-is-a-nonprofit-organization_1600x.jpg?v=1650520340",
-      description: "Generic Nonprofit Image"
-    },
-    location: {
-      address: "100 Abby Lane",
-      city: "Syosset",
-      state: "NY",
-      zip: "11797",
-    },
-    date_created: new Date(),
-    date_hosted: new Date(),
-    main: ["Pet companion", "Lonely souls", "Animal Rescue"],
-    mission: "Our mission is to ...", 
-    description:
-        "This is non-profit org which works with Climate",
-    cause_areas: ["Advocacy", "Community"],
-    skills: ["Communication", "People Skills", "Social Work"],
-    good_for:["Group"],
-    requirements:["Requirement 1", "Requirement 2"],
-  },
-  {
-    id: 3,
-    name: "Forver Paws Animal Shelter",
-    image: {
-      url: "https://www.raisely.com/blog/content/images/2023/01/Asset-1-1.png",
-      description: "Generic Nonprofit Image"
-    },
-    location: {
-      address: "123 Main Street",
-      city: "Fall River",
-      state: "MA",
-      zip: "02121"
-    },
-    date_created: new Date(),
-    date_hosted: new Date(),
-    main: ["Animal Rescue", "Pet Adoption"],
-    mission: "Our mission is to ...", 
-    description:
-      "This nonprofit gives animals a second chance at finding a furever home.",
-    cause_areas: ["Advocacy", "Community"],
-    skills: ["Communication", "People Skills", "Social Work"],
-    good_for: "Group",
-    requirements: ["Requirement 1", "Requirement 2"]
-  },
-  {
-    id: 4,
-    name: "Gift of Bread",
-    image: {
-      url: "https://www.glenroybakery.com.au//wp-content/uploads/2023/01/Wholesale-Bread-Glenroy-Bakery.jpg", 
-      description: "Picture of different types of bread"
-    },
-    location: {
-      address: "79 George Street",
-      city: "Sydney",
-      state: "NSW",
-      zip: "2001"
-    },
-    date_created: new Date(),
-    date_hosted: new Date(),
-    main: ["Sharing bread", "Reduce food waste"],
-    mission: "Committed to building strong, inclusive communities while also minimizing waste and promoting sustainability", 
-    description:
-      "Gift of Bread is an Australian food rescue charitable organisation",
-    cause_areas: ["Sustainability", "Feeding vulnerable people"],
-    skills: ["Baking", "Food handling", "First aid"],
-    good_for: "Group",
-    requirements: ["Availability for night shifts", "Driving Licence"]
-  },
-  {
-    id: 5,
-    name: "American  Red Cross",
-    image:  {
-      url: "https://www.redcross.org/content/dam/redcross/donations/holiday/2023-holiday-catalog/2023-holiday-donate-form-creative/Holiday-2023-WINM-Desktop_1348x505.png.transform/1024/q70/feature/image.png",
-      description: "red cross"
-    },
-
-    location: {
-      address: "123 South Street",
-      city: "St. Louis",
-      state: "MO",
-      zip: "123456"
-    },
-    date_created: new Date(),
-    date_hosted: new Date(),
-    main: ["Donate blood", "Save lives"],
-    mission: "Our mission is to ...",
-    description:
-      "Humanitarian organization that provides emergency assistance, disaster relief, and disaster preparedness",
-    cause_areas: ["Aid", "Humanitarian"],
-    skills: ["Leadership", "People Skills", "Social Work"],
-    good_for: "Group",
-    requirements: ["good blood", "good heart"],
-  },
-  {
-    id: 6,
-    name: "Canine Sanctuary",
-    image: {
-      url: "https://www.rescuedogs.co.uk/wp-content/uploads/2021/02/pebbles-thumbnail.jpg",
-      description: "Picture of a lovely dog"
-    },
-    location: {
-      address: "42-04 Crescent Ave",
-      city: "Queens",
-      state: "NY",
-      zip: "11106"
-    },
-    date_created: new Date(),
-    date_hosted: new Date(),
-    main: ["Dog Rescue", "Dog Rehabilitiation"],
-    mission: "Our mission is to reduce the instances of dog fighting rings to zero and rescue and rehabilitate dogs.", 
-    description:
-      "This nonprofit investigates dog fighting rings and organizes animal rescue operations. ",
-    cause_areas: ["Animal Rights", "Illegal Operations", "Animal Rehabilitation Center", "Animal Sanctuary"],
-    skills: ["Communication", "Pet Handling", "Social Work", "Military experience", "Law Enforcement Experience"],
-    good_for: "Group",
-    requirements: ["Social work experience", "Animal care experience"]
-  }
-];
-
+import { volunteer_list } from "./data.js";
+import { renderHomeMarkup } from "./views/homeView.js";
 /******************************************************
  * * * * * * * * * * * * * HOME * * * * * * * * * * * *
  ******************************************************/
 
 const renderHome = () => {
+  // Target root element
   let main = document.getElementById("root");
-  let markup = `
-    <div class="volunteerHome" id="volunteerHome">
-      <div class="volunteerHome__image">Image</div>
-      <h2 class="volunteerHome__header">Find Volunteer Opportunities</h2>
-      <p class="volunteerHome__description">We make it easy for people who want to get involved. Signing up is just a click away.</p>
-      <button class="volunteerHome__button" id="volunteerHome__button">Explore</button>
-    </div>
-  `;
-  // Insert our home page into our main container
+  // Store markup in a variable
+  let markup = renderHomeMarkup();
+  // Insert our home markup into our root
   main.innerHTML = markup;
-  
+  // Add event listeners
   const volunteerButton = document.getElementById("volunteerHome__button");
   volunteerButton.addEventListener("click", renderVolunteerList);
 }
@@ -225,16 +50,6 @@ const renderVolunteerList = () => {
   searchResults.addEventListener("click", renderVolunteerDetails);
 }
 
-const getSkills = (vol) => {
-  let skillDiv = `<div class="volunteerCard__skills">
-                    <span class="volunteerCard__skills--label">
-                      Skills You'll Gain: 
-                    </span>
-                    ${vol.skills.join(', ')}
-                  </div>`;
-  return skillDiv;
-};
-
 const renderCard = (vol) => {
   return `
     <div data-id="${vol.id}" class="volunteerCard"> 
@@ -251,6 +66,16 @@ const renderCard = (vol) => {
     </div>
   `;
 }
+
+const getSkills = (vol) => {
+  let skillDiv = `<div class="volunteerCard__skills">
+                    <span class="volunteerCard__skills--label">
+                      Skills You'll Gain: 
+                    </span>
+                    ${vol.skills.join(', ')}
+                  </div>`;
+  return skillDiv;
+};
 
 //function to render the search results
 function showSearchResults() {
@@ -318,7 +143,6 @@ const renderVolunteerDetails = (e) => {
  * * * * * * * * * * * * CORE * * * * * * * * * * * * * 
  ******************************************************/
 
-// showDiv("volunteer_search_home");
 window.addEventListener("load", renderHome);
 
 // Volunteer nav button functionality
